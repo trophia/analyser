0839e71a4b2ed9783bffc6a82593bb036054c953
Nokome Bentley <nokome.bentley@trophia.com>
2016-06-08 14:33:44 +1200

    Add strata_plot

--- R/aggregater.r
+++ R/aggregater.r
@@ -33,7 +33,10 @@ Aggregater <- function(data_in,by){
       shared_fyear_summary(data)
     }
 
-    rollup_plots <- function(){
+    #' Rollup plots summarise the degree of 'rolling up' done by aggreagation
+    #' and may indicate reporting behaviour in terms of events by strata (as defined)
+    #' and catches by strata (compared to by events and trips)
+    rollup_plot <- function(){
       temp1 <- data_in %>% group_by(fyear) %>% summarise(
         events_positive = length(unique(event[catch>0]))/length(unique(event))*100
       )
@@ -57,7 +60,60 @@ Aggregater <- function(data_in,by){
         scale_shape_manual(values=1:3) +
         labs(x='Fishing year', y='Percentage with positive catch',shape='',colour='')
 
-      list(events=plot1, positive=plot2)
+      grid.arrange(plot1, plot2)
+    }
+
+    #' Strata plot examine alternative stratum defintions
+    strata_plot <- function(){
+
+      # Determine which combinations of factors to use as potential
+      # strata definitions
+      combos <- c('trip','trip*date')
+      methods <- length(unique(data_in$method))>1
+      areas <- length(unique(data_in$area))>1
+      targets <- length(unique(data_in$target))>1
+      if(methods) combos <- c(combos, 'trip*date*method')
+      if(areas) combos <- c(combos, 'trip*date*area')
+      if(targets) combos <- c(combos, 'trip*date*target')
+      if(methods & areas) combos <- c(combos, 'trip*date*method*area')
+      if(methods & targets) combos <- c(combos, 'trip*date*method*target')
+      if(areas & targets) combos <- c(combos, 'trip*date*area*target')
+      if(methods & areas & targets) combos <- c(combos, 'trip*date*method*area*target')
+
+      # For each combination, do an aggregation and summarise
+      # effort and catch by stratum
+      temp_all <- NULL
+      for(combo in combos){
+        combo_factors <- strsplit(combo, '\\*')[[1]]
+        temp <- data_in %>%
+          group_by_(.dots = c('fyear',combo_factors)) %>%
+          summarise(
+            effort = sum(effort, na.rm = TRUE),
+            catch = sum(catch, na.rm = TRUE)
+          ) %>%
+            group_by(fyear) %>%
+            summarise(
+              effort = mean(effort),
+              catch = mean(catch>0)
+            )
+        temp$combo <- combo
+        temp_all <- bind_rows(temp_all,temp)
+      }
+
+      plot1 <- ggplot(temp_all, aes(x=fyear, y=effort, color=combo, shape=combo)) +
+        geom_point() + geom_line() +
+        ylim(0,NA) +
+        labs(x='Fishing year', y='Mean effort units per stratum',color='Stratum definition',shape='Stratum definition') +
+        theme(legend.position='top')
+
+      plot2 <- ggplot(temp_all, aes(x=fyear, y=catch, color=combo, shape=combo)) +
+        geom_point() + geom_line() + 
+        ylim(0,NA) +
+        labs(x='Fishing year', y='Strata with catch (%)') +
+        theme(legend.position='none')
+
+      grid.arrange(plot1, plot2, heights = c(0.6, 0.4))
+
     }
 
     environment()

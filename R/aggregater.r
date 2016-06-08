bd653cc0ca9daa2578526263b0684dc9d96777fc
Nokome Bentley <nokome.bentley@trophia.com>
2016-06-08 13:05:41 +1200

    Adds rollup plots

--- R/aggregater.r
+++ R/aggregater.r
@@ -33,5 +33,32 @@ Aggregater <- function(data_in,by){
       shared_fyear_summary(data)
     }
 
+    rollup_plots <- function(){
+      temp1 <- data_in %>% group_by(fyear) %>% summarise(
+        events_positive = length(unique(event[catch>0]))/length(unique(event))*100
+      )
+      temp2 <- data %>% group_by(fyear) %>% summarise(
+        events_per_stratum = sum(events)/length(events),
+        strata_positive = sum(catch>0)/length(catch)*100,
+        trips_positive = length(unique(trip[catch>0]))/length(unique(trip))*100
+      )
+      temp <- left_join(temp1, temp2)
+
+      plot1 <- ggplot(temp,aes(x=fyear,y=events_per_stratum)) +
+        geom_point() + geom_line() + 
+        ylim(0,NA) + 
+        labs(x='Fishing year', y='Events per stratum')
+
+      plot2 <- ggplot(temp,aes(x=fyear)) +
+        geom_point(aes(y=events_positive,colour='Events',shape='Events')) + geom_line(aes(y=events_positive,colour='Events')) + 
+        geom_point(aes(y=strata_positive,colour='Strata',shape='Strata')) + geom_line(aes(y=strata_positive,colour='Strata')) + 
+        geom_point(aes(y=trips_positive,colour='Trips',shape='Trips')) + geom_line(aes(y=trips_positive,colour='Trips')) + 
+        ylim(0,NA) + 
+        scale_shape_manual(values=1:3) +
+        labs(x='Fishing year', y='Percentage with positive catch',shape='',colour='')
+
+      list(events=plot1, positive=plot2)
+    }
+
     environment()
 }

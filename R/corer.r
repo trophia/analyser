3fa7aeae84c5a9193afe629273b5c9d71b7943ab
Nokome Bentley <nokome.bentley@trophia.com>
2016-06-10 15:26:19 +1200

    Add CPUE plot

--- R/corer.r
+++ R/corer.r
@@ -1,6 +1,3 @@
-library(dplyr)
-library(ggplot2)
-
 #' Select a core vessel set
 #' 
 #' @param data Data to operate on
@@ -67,14 +64,18 @@ Corer <- function(data_in, catch_min=1, trips_min=3, years_min=3){
     criteria_vessels <- within(criteria_vessels, {
       catch <- catch / sum(data_in$catch) * 100
     })
+
     # Plot it
     plot_base <- ggplot(criteria_vessels,aes(x=years_min,colour=factor(trips_min),shape=factor(trips_min))) + 
       scale_shape_manual(values=1:10) +
       labs(x='Minimum year', colour='Minimum trips', shape='Minimum trips')
+
     plot1 <- plot_base + geom_point(aes(y=catch),size=2,alpha=0.7) + 
       ylim(0,NA) + labs(x='', y='Percentage of catch') + theme(legend.position='top')
+
     plot2 <- plot_base + geom_point(aes(y=num),size=2,alpha=0.7) +
       ylim(0,NA) + labs(y='Number of vessels') + theme(legend.position='none')
+
     grid.arrange(plot1, plot2, heights = c(0.6, 0.5))
   }
   
@@ -86,10 +87,52 @@ Corer <- function(data_in, catch_min=1, trips_min=3, years_min=3){
         (temp %>% group_by(vessel) %>% summarise(last=max(fyear),first=min(fyear)) %>% arrange(last,first))$vessel
       )
     )
+
     ggplot(temp,aes(x=fyear,y=vessel,size=trips)) +
       geom_point(alpha=0.5) + scale_size_area(max_size = 10) + 
       labs(x='Fishing year', y='Vessel', size='Trips') +
       theme(axis.text.y=element_text(size=0))
+  }
+
+  cpue_plot <- function(){
+    temp <- bind_rows(
+      data_in %>%
+        mutate(
+          vessels = ifelse(vessel %in% vessels, 'core', 'others')
+        ) %>%
+        group_by(fyear,vessels) %>%
+        summarise(
+          num = length(effort),
+          caught = mean(catch>0),
+          rate = geomean(catch/effort)
+        ),
+      data_in %>%
+        mutate(
+          vessels = 'all'
+        ) %>%
+        group_by(fyear,vessels) %>%
+        summarise(
+          num = length(effort),
+          caught = mean(catch>0),
+          rate = geomean(catch/effort)
+        )
+    )
+
+    plot1 <- ggplot(temp, aes(x=fyear, y=caught, colour=vessels, shape=vessels)) +
+      geom_point(aes(size=num)) + geom_line() +
+      scale_size_area() +
+      ylim(0,NA) + 
+      scale_shape_manual(values=1:3) +
+      labs(x='', y='Catch probability (proportion catch>0)', colour='', shape='', size='Strata')
+
+    plot2 <- ggplot(temp, aes(x=fyear, y=rate, colour=vessels, shape=vessels)) +
+      geom_point(aes(size=num)) + geom_line() +
+      scale_size_area() +
+      ylim(0,NA) +
+      scale_shape_manual(values=1:3) +
+      labs(x='Fishing year', y='Catch magnitude (kg per effort unit)', colour='', shape='', size='Strata')
+
+    grid.arrange(plot1, plot2, heights = c(0.5, 0.5))
   }
   
   environment()

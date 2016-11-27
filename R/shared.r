ba7a9c2a3f8054f56ffe61f34320844d8ded066f
Max Schofield <max.schofield@tridentsystems.co.nz>
2016-11-27 16:34:51 +1300

    tweak names for less confusion

--- R/shared.r
+++ R/shared.r
@@ -8,17 +8,17 @@ shared_fyear_summary <- function(data){
     p1 <- data %>% group_by('Fishing year' = fyear) %>% summarise(
         Vessels = length(unique(vessel)),
         Trips = length(unique(trip)),
-        Events = round(sum(events),0),
+        Records = round(sum(events),0),
         'Effort (num)'=sum(num,na.rm=T),
         'Duration (hrs)'= round(sum(duration,na.rm=T),0),
         'Catch (t)'= round(sum(catch,na.rm=T)/1000, 1),
-        'Catch Positive'= round(sum(catch>0,na.rm=T)/length(catch)*100,2)
+#         'Catch Positive'= round(length(catch>0)/length(catch)*100,2)
     )
     p2 <- data %>% group_by(fyear, trip) %>% summarise('positive' = ifelse(sum(catch) > 0, 1, 0)) %>%
         group_by(fyear) %>% summarise('Trips caught' = round((sum(positive) / n_distinct(trip)) * 100,2))
 
     p3 <- data %>% group_by(fyear, trip, date) %>% summarise('positive' = ifelse(sum(catch) > 0, 1, 0)) %>%
-        group_by(fyear) %>% summarise('Events caught' = round((sum(positive) / length(positive)) * 100, 2))
+        group_by(fyear) %>% summarise('Days caught' = round((sum(positive) / length(positive)) * 100, 2))
 
     bind_cols(p1, p2[ , 2], p3[ , 2])
 }
